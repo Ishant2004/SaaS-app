a0ca7dad5dd947ea8e3fb93f41990f6c47362db9
Ishant2004 <ishant.rohilla.2004@gmail.com>
2025-06-02 11:45:23 +0530

    commit 2nd june afternoon

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import {Button} from "@/components/ui/button";
+
 
 const Page = () => {
   return (
-    <div>Page</div>
+
+        <div><h1 className="text-2xl underline">this is a hedding</h1>
+        <Button>hello</Button>
+        </div>
+
   )
 }
 
