37324159d8f8fcc3b3ac57e2fb273511170921de
Ishant2004 <ishant.rohilla.2004@gmail.com>
2025-06-08 19:03:59 +0530

    commit 8th evening added my-journey page and the clerk subscription plan features

--- lib/actions/companion.actions.ts
+++ lib/actions/companion.actions.ts
@@ -73,7 +73,7 @@ export const getRecentSessions = async (limit = 10) => {
     const supabase = createSupabaseClient();
     const { data, error } = await supabase
         .from('session_history')
-        .select(`companions:comanion_id (*)`)
+        .select(`companions:companion_id (*)`)
         .order('created_at', {ascending: false })
         .limit(limit)
 
@@ -86,7 +86,7 @@ export const getUserSessions = async (userId: string, limit = 10) => {
     const supabase = createSupabaseClient();
     const { data, error } = await supabase
         .from('session_history')
-        .select(`companions:comanion_id (*)`)
+        .select(`companions:companion_id (*)`)
         .eq('user_id', userId)
         .order('created_at', {ascending: false })
         .limit(limit)
@@ -94,4 +94,47 @@ export const getUserSessions = async (userId: string, limit = 10) => {
     if(error) throw new Error(error.message);
 
     return data.map(({companions}) => companions);
+}
+
+export const getUserCompanions = async (userId: string) => {
+    const supabase = createSupabaseClient();
+    const { data, error } = await supabase
+        .from('companions')
+        .select()
+        .eq('author', userId)
+
+    if(error) throw new Error(error.message);
+
+    return data;
+}
+
+export const newCompanionPermissions = async () => {
+
+    const supabase = createSupabaseClient();
+    const { userId, has } = await auth();
+
+    let limit = 0;
+
+    if(has({ plan: 'pro'})) {
+        return true;
+    } else if(has({ feature: '3_companion_limit' })) {
+        limit = 3;
+    } else if(has({ feature: '10_companion_limit' })) {
+        limit = 10;
+    }
+
+    const { data, error } = await supabase
+        .from('companions')
+        .select('id', {count: 'exact'})
+        .eq('author', userId)
+
+    if(error) throw new Error(error.message);
+
+    const companionCount = data?.length;
+
+    if(companionCount >= limit) {
+        return false;
+    } else {
+        return true;
+    }
 }
